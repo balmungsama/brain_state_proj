039ff6d973e4124586c9d3caa7bad5022658f9da
BalmungSama <jbryaneusebio@gmail.com>
2017-02-06 20:13:42 -0500

    Added plotting functionality

--- Scripts/Dependencies/avg_ppi_mat.R
+++ Scripts/Dependencies/avg_ppi_mat.R
@@ -2,9 +2,11 @@ args <- commandArgs()
 
 TOP_DIR  <- args[6]
 TASK_NM  <- args[7]
+
 COUNT    <- 0
 mat_path <- 'ppi_results/construct_matrix/matrix'
-out_path <- file.path(TOP_DIR, 'whole_brain_ppi', paste0(TASK_NM, '_avg.csv') )
+out_path_csv <- file.path(TOP_DIR, 'whole_brain_ppi', paste0(TASK_NM, '_avg.csv') )
+out_path_png <- file.path(TOP_DIR, 'whole_brain_ppi', paste0(TASK_NM, '_avg.png') )
 
 dir.create( file.path(TOP_DIR, 'whole_brain_ppi') )
 subj_ls <- dir(TOP_DIR, all.files = F, recursive = F)
@@ -24,4 +26,31 @@ for(subj in subj_ls) {
 
 avg_mat <- avg_mat/COUNT
 
-write.csv( x = avg_mat, file = out_path, row.names = dim(avg_mat)[1], col.names = dim(avg_mat)[2] )
+##### Save the average matrix to a *.csv file #####
+write.csv( x = avg_mat, file = out_path_csv, row.names = dim(avg_mat)[1], col.names = dim(avg_mat)[2] )
+
+##### Create a plot of the average matrix and save it as a .png file #####
+
+par(mar=c(7,4,4,2)+0.1)
+
+png(filename = out_path_png, width = 1500, height = 1500)
+
+colfunc <- colorRampPalette(c('#426DED', '#EDE742', '#ED4242')) # the colours defining the range to be used
+heat_cols <- colfunc(100)  #  gradient of 100 colours b/w the 3 key colours defined above
+
+heatmap.2(ppi_mat,
+		  main = paste0(TASK_NM, ' - ** AVERAGED ** : whole-brain FC'),
+		  trace = 'none',
+		  Rowv = F,
+		  Colv = F,
+		  scale = 'none',
+		  key = T,
+		  density.info = 'none',
+		  dendrogram = 'none',
+		  key.xlab = NA,
+		  key.title = NA,
+		  # labRow = labels,
+		  # labCol = labels,
+		  col = heat_cols)
+
+dev.off()
