f10dc51b47e0fc8ebdd391ceed68fd2784d5ce4f
BalmungSama <jbryaneusebio@gmail.com>
2017-04-28 17:14:41 -0400

    changed commenting to run everything after normalization

--- Preprocessing/preproc_wrapper.sh
+++ Preprocessing/preproc_wrapper.sh
@@ -13,14 +13,14 @@ PREPROC='/home/hpc3586/brain_state_proj/Preprocessing'
 
 echo INITIALIZING PREPROCESSING --------------------
 
-bash $PREPROC/skullstrip.sh $SUBJ_DIR $COND $PREPROC
-bash $PREPROC/slicetime.sh $SUBJ_DIR $COND $PREPROC
-bash $PREPROC/motcor.sh $SUBJ_DIR $COND $PREPROC
-bash $PREPROC/normalization.sh $SUBJ_DIR $COND $PREPROC
-# bash $PREPROC/motreg.sh $SUBJ_DIR $COND $PREPROC
-# Rscript $PREPROC/deg2mm.R --PATH=$SUBJ_DIR --COND=$COND
-# Rscript $PREPROC/framewise_disp.R --PATH=$SUBJ_DIR --COND=$COND
-# bash $PREPROC/meantsBOLD.sh $SUBJ_DIR $COND $PREPROC
-# Rscript $PREPROC/multiplot.R --PATH=$SUBJ_DIR --COND=$COND
-# Rscript $PREPROC/mot_summ.R --PATH=$SUBJ_DIR --COND=$COND
-# Rscript $PREPROC/scrubbing.R --PATH=$SUBJ_DIR --COND=$COND --FD=$FD --DVARS=$DVARS --RM=$RM
+# bash $PREPROC/skullstrip.sh $SUBJ_DIR $COND $PREPROC
+# bash $PREPROC/slicetime.sh $SUBJ_DIR $COND $PREPROC
+# bash $PREPROC/motcor.sh $SUBJ_DIR $COND $PREPROC
+# bash $PREPROC/normalization.sh $SUBJ_DIR $COND $PREPROC
+bash $PREPROC/motreg.sh $SUBJ_DIR $COND $PREPROC
+Rscript $PREPROC/deg2mm.R --PATH=$SUBJ_DIR --COND=$COND
+Rscript $PREPROC/framewise_disp.R --PATH=$SUBJ_DIR --COND=$COND
+bash $PREPROC/meantsBOLD.sh $SUBJ_DIR $COND $PREPROC
+Rscript $PREPROC/multiplot.R --PATH=$SUBJ_DIR --COND=$COND
+Rscript $PREPROC/mot_summ.R --PATH=$SUBJ_DIR --COND=$COND
+Rscript $PREPROC/scrubbing.R --PATH=$SUBJ_DIR --COND=$COND --FD=$FD --DVARS=$DVARS --RM=$RM
