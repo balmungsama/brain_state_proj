ffa152ae21f53d3a6795ad803f42f539ef853f59
BalmungSama <jbryaneusebio@gmail.com>
2017-04-28 13:40:54 -0400

    Switched to FSL (from AFNI)

--- Preprocessing/motreg.sh
+++ Preprocessing/motreg.sh
@@ -12,25 +12,4 @@ NUM_CORES=$(grep -c ^processor /proc/cpuinfo)
 
 echo '	motion regression...'
 
-3dDeconvolve                                                       \
-	-input $SUBJ_DIR/task_data/preproc/snlmt_$COND.nii*                                 \
-																																	 \
-	-num_stimts 6                                                    \
-	-stim_file 1 $SUBJ_DIR/MPEs/$COND.1D'[0]' -stim_base 1 -stim_label 1 roll  \
-	-stim_file 2 $SUBJ_DIR/MPEs/$COND.1D'[1]' -stim_base 2 -stim_label 2 pitch \
-	-stim_file 3 $SUBJ_DIR/MPEs/$COND.1D'[2]' -stim_base 3 -stim_label 3 yaw   \
-	-stim_file 4 $SUBJ_DIR/MPEs/$COND.1D'[3]' -stim_base 4 -stim_label 4 dS    \
-	-stim_file 5 $SUBJ_DIR/MPEs/$COND.1D'[4]' -stim_base 5 -stim_label 5 dL    \
-	-stim_file 6 $SUBJ_DIR/MPEs/$COND.1D'[5]' -stim_base 6 -stim_label 6 dP    \
-	                                                                 \
-	-fitts $SUBJ_DIR/task_data/preproc/fmt_$COND                                                 \
-	-errts $SUBJ_DIR/task_data/preproc/emt_$COND                                                 \
-  # -tout                                                            \
-  # -x1D $COND.xmat.1D                                               \
-	# -xjpeg $COND.jpg                                                 \
-  # -jobs $NUM_CORES
-
-3dAFNItoNIFTI -prefix $SUBJ_DIR/task_data/preproc/emt_$COND $SUBJ_DIR/task_data/preproc/emt_$COND+*.HEAD
-3dAFNItoNIFTI -prefix $SUBJ_DIR/task_data/preproc/fmt_$COND $SUBJ_DIR/task_data/preproc/fmt_$COND+*.HEAD
-rm $SUBJ_DIR/task_data/preproc/emt_$COND+*.*
-rm $SUBJ_DIR/task_data/preproc/fmt_$COND+*.*
+fsl_glm -i $SUBJ_DIR/task_data/preproc/snlmt_$COND.nii* -d $SUBJ_DIR/MPEs/$COND.1D --out_res=$SUBJ_DIR/task_data/preproc/motreg_snlmt_$COND.nii*
