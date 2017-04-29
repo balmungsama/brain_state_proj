27012d6e5bea566db39cad99aaac989025e58a38
BalmungSama <jbryaneusebio@gmail.com>
2017-04-28 20:36:32 -0400

    switched slice time correction to fsl

--- Preprocessing/slicetime.sh
+++ Preprocessing/slicetime.sh
@@ -8,7 +8,9 @@ echo '	slice-time correction...'
 
 TR=$(3dinfo -tr $SUBJ_DIR/task_data/$COND.nii*)
 
-3dTshift -TR $TR's' -prefix $SUBJ_DIR/task_data/preproc/t_$COND $SUBJ_DIR/task_data/$COND.nii*
+slicetimer -i $SUBJ_DIR/task_data/$COND -o $SUBJ_DIR/task_data/preproc/t_$COND -r $TR
 
-3dAFNItoNIFTI -prefix $SUBJ_DIR/task_data/preproc/t_$COND $SUBJ_DIR/task_data/preproc/t_$COND+orig.HEAD
-rm $SUBJ_DIR/task_data/preproc/t_$COND+orig.*
+# 3dTshift -TR $TR's' -prefix $SUBJ_DIR/task_data/preproc/t_$COND $SUBJ_DIR/task_data/$COND.nii*
+
+# 3dAFNItoNIFTI -prefix $SUBJ_DIR/task_data/preproc/t_$COND $SUBJ_DIR/task_data/preproc/t_$COND+orig.HEAD
+# rm $SUBJ_DIR/task_data/preproc/t_$COND+orig.*
