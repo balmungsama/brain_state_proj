cb05e260822da555b37a080fd0da68922bec2109
BalmungSama <jbryaneusebio@gmail.com>
2017-04-29 16:44:26 -0400

    checking framewise_disp.R

--- Preprocessing/preproc_wrapper.sh
+++ Preprocessing/preproc_wrapper.sh
@@ -18,8 +18,8 @@ echo INITIALIZING PREPROCESSING --------------------
 # bash $PREPROC/motcor.sh $SUBJ_DIR $COND $PREPROC
 # bash $PREPROC/normalization.sh $SUBJ_DIR $COND $PREPROC
 # bash $PREPROC/motreg.sh $SUBJ_DIR $COND $PREPROC
-Rscript $PREPROC/deg2mm.R --PATH=$SUBJ_DIR --COND=$COND
-# Rscript $PREPROC/framewise_disp.R --PATH=$SUBJ_DIR --COND=$COND
+# Rscript $PREPROC/deg2mm.R --PATH=$SUBJ_DIR --COND=$COND
+Rscript $PREPROC/framewise_disp.R --PATH=$SUBJ_DIR --COND=$COND
 # bash $PREPROC/meantsBOLD.sh $SUBJ_DIR $COND $PREPROC
 # Rscript $PREPROC/multiplot.R --PATH=$SUBJ_DIR --COND=$COND
 # Rscript $PREPROC/mot_summ.R --PATH=$SUBJ_DIR --COND=$COND
