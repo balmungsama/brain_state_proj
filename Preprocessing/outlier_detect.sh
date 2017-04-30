09984ebb3c6bd84e58ff49b75df1cc97f488fb74
BalmungSama <jbryaneusebio@gmail.com>
2017-04-29 23:10:25 -0400

    create plot directory

--- Preprocessing/outlier_detect.sh
+++ Preprocessing/outlier_detect.sh
@@ -1,11 +1,15 @@
 SUBJ_DIR=$1
 COND=$2
 PREPROC=$3
+RM=$RM
 
 echo '	computing DVARS...'
 
+mkdir $SUBJ_DIR/mot_analysis/plots
+
 NIFTI_FILE=$SUBJ_DIR/task_data/preproc/motreg_snlmt_$COND
 
-fsl_motion_outliers -i $NIFTI_FILE -o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' -s $SUBJ_DIR/mot_analysis/$COND'_DVARS.txt' -p $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' --dvars 
+fsl_motion_outliers -i $NIFTI_FILE -o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' -s $SUBJ_DIR/mot_analysis/$COND'_DVARS.txt' -p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS.val' --dvars 
 
-fsl_motion_outliers -i $NIFTI_FILE -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.txt' -p $SUBJ_DIR/mot_analysis/$COND'_FD.val' --fd
+fsl_motion_outliers -i $NIFTI_FILE -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.txt' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD.val' --fd
+
