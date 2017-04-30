97b0b2a311a07b498c1c133afc732b57f2f76e38
BalmungSama <jbryaneusebio@gmail.com>
2017-04-30 19:52:16 -0400

    added digit padding

--- Scripts/Dependencies/k_means_clustering_beta3.R
+++ Scripts/Dependencies/k_means_clustering_beta3.R
@@ -113,8 +113,8 @@ if(missing_requirements > 0) {
 	    roi_cormat <- roi_tcourses[ win_start:(win_start + win_sz) , ]
 	    roi_cormat <- cor(roi_cormat)
 	    
-	    print( file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', win_start, '_', (win_start + win_sz), '.csv') ) )
-	    write.csv(roi_cormat, file = file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', str_pad(win_start, nchar(dim(roi_tcourses)[1]), pad = 0), '_', str_pad(win_start + win_sz, dim(roi_tcourses)[1], pad = 0), '.csv') ), row.names = T, col.names = T )
+	    print( file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', str_pad(win_start, nchar(dim(roi_tcourses)[1]), pad = 0), '_', str_pad((win_start + win_sz), dim(roi_tcourses)[1], pad = 0), '.csv') ) )
+	    write.csv(roi_cormat, file = file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', str_pad(win_start, nchar(dim(roi_tcourses)[1]), pad = 0), '_', str_pad((win_start + win_sz), dim(roi_tcourses)[1], pad = 0), '.csv') ), row.names = T, col.names = T )
 	    
 	    win_start <- win_start + 1
 	  }
