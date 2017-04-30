01249cb7779732d41bf854642f23286af22d2fb0
BalmungSama <jbryaneusebio@gmail.com>
2017-04-30 13:36:30 -0400

    changed file name to be consistent with new processing pipeline

--- Scripts/Dependencies/sliding_timewin.sh
+++ Scripts/Dependencies/sliding_timewin.sh
@@ -33,7 +33,7 @@ if [[ $mode == "subj" ]]; then
 	for roi in $(ls $ROI_DIR); do
 		roi=${roi%'.nii.gz'*}
 
-		fslmeants -i ../task_data/preproc/snlmt_$cond.nii -m $ROI_DIR/$roi'.nii'* -o $roi'_tcourse.txt'
+		fslmeants -i ../task_data/preproc/scrubbed_motreg_snlmt_$cond.nii -m $ROI_DIR/$roi'.nii'* -o $roi'_tcourse.txt'
 
 	done
 
@@ -50,7 +50,7 @@ elif [[ $mode == "group" ]]; then
 			for roi in $(ls $ROI_DIR); do
 				# echo $roi
 				roi=${roi%'.nii.gz'*}
-				fslmeants -i ../task_data/preproc/snlmt_$cond.nii -m $ROI_DIR/$roi'.nii'* -o $roi'_tcourse.txt'
+				fslmeants -i ../task_data/preproc/scrubbed_motreg_snlmt_$cond.nii -m $ROI_DIR/$roi'.nii'* -o $roi'_tcourse.txt'
 			done
 
 			cd $path
