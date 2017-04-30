9a392926ee761ed484bfe37c9c0d7359e722e2e4
BalmungSama <jbryaneusebio@gmail.com>
2017-04-30 19:53:53 -0400

    fixed nchar padding

--- Scripts/Dependencies/k_means_clustering_beta3.R
+++ Scripts/Dependencies/k_means_clustering_beta3.R
@@ -114,7 +114,7 @@ if(missing_requirements > 0) {
 	    roi_cormat <- cor(roi_cormat)
 	    
 	    print( file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', str_pad(win_start, nchar(dim(roi_tcourses)[1]), pad = 0), '_', str_pad((win_start + win_sz), nchar(dim(roi_tcourses)[1]), pad = 0), '.csv') ) )
-	    write.csv(roi_cormat, file = file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', str_pad(win_start, nchar(dim(roi_tcourses)[1]), pad = 0), '_', str_pad((win_start + win_sz), dim(roi_tcourses)[1], pad = 0), '.csv') ), row.names = T, col.names = T )
+	    write.csv(roi_cormat, file = file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', paste0('win_', str_pad(win_start, nchar(dim(roi_tcourses)[1]), pad = 0), '_', str_pad((win_start + win_sz), nchar(dim(roi_tcourses)[1]), pad = 0), '.csv') ), row.names = T, col.names = T )
 	    
 	    win_start <- win_start + 1
 	  }
