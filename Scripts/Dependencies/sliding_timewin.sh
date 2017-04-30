0d3baf9e707763bdf2f9f52b88dfdd11af24ba2b
BalmungSama <jbryaneusebio@gmail.com>
2017-04-30 16:10:41 -0400

    added echo statements

--- Scripts/Dependencies/sliding_timewin.sh
+++ Scripts/Dependencies/sliding_timewin.sh
@@ -21,11 +21,18 @@ path=$4
 ROI_DIR=$5
 cond=$6
 
+echo "Calculating ROI time-courses..."
+echo " "
 
 for roi in $(ls $ROI_DIR); do
-	
+
+	echo "	- running $roi"
+
 	roi=${roi%'.nii.gz'*}	
 	
 	fslmeants -i $path/task_data/preproc/scrubbed_motreg_snlmt_$cond.nii -m $ROI_DIR/$roi'.nii'* -o $path/roi_tcourses/$roi'_tcourse.txt'	
 
 done
+
+echo " "
+echo "Finished."
