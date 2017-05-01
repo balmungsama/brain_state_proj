7eb2757498e1ff385fce1f58ecd50355e4322179
BalmungSama <jbryaneusebio@gmail.com>
2017-05-01 00:59:43 -0400

    added the update to teh loop

--- Scripts/Dependencies/k_means_clustering_CAC.R
+++ Scripts/Dependencies/k_means_clustering_CAC.R
@@ -155,15 +155,16 @@ if(missing_requirements > 0) {
 	        write.csv(x = invalid.row, file = file.path(TOP_DIR, '..', 'kmeans', 'invalid_rows.csv'), append = T)
 	        
 	        
+	      } else {
+	        write.table( x = row.cormat, file = file.path(TOP_DIR, '..', 'kmeans', 'correl_rows.csv'), append = T, row.names = F, col.names = F, sep = ',' )
 	      }
 	      
 	      # if ( sum(row.cormat > 1) > 0) {
 	      #   print('some values are greaater than one')
 	      # }
 	      
-	      write.table( x = row.cormat, file = file.path(TOP_DIR, '..', 'kmeans', 'correl_rows.csv'), append = T, row.names = F, col.names = F, sep = ',' )
+	     
 	    }
-	    
 	    
 	    write.csv(roi_cormat, 
 	              file = file.path(TOP_DIR, subj, 'roi_tcourses', 'cor_mats', 
@@ -173,7 +174,7 @@ if(missing_requirements > 0) {
 	              row.names = labels, 
 	              col.names = labels )
 	    
-	    
+	    win_start <- win_start + 1
 	  }
 	  
 	  subj_count <- subj_count + 1
