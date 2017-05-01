eea37e4480bb0d6a4e4153600569f24af2a2c1cf
BalmungSama <jbryaneusebio@gmail.com>
2017-05-01 03:01:18 -0400

    kmeans logs

--- Scripts/R_in_CAC.sh
+++ Scripts/R_in_CAC.sh
@@ -4,6 +4,9 @@
 #$ -cwd
 #$ -M hpc3586@localhost
 #$ -m be
+#$ -o /home/hpc3586/brain_state_proj/Scripts/k_logs/STD.out
+#$ -e /home/hpc3586/brain_state_proj/Scripts/k_logs/STD.err
+
 
 rscript=$1
 
