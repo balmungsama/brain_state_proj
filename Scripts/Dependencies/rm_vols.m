509ff1007bbdcde85aa4e0a3432d2b6b130397ba
BalmungSama <jbryaneusebio@gmail.com>
2017-05-29 16:24:04 -0400

    These scripts will remove empty volumes from the scrubbed time sries, which is required for the ICA implementation used in MELODIC

---
+++ Scripts/Dependencies/rm_vols.m
@@ -0,0 +1,17 @@
+
+confound = fullfile(TOP_DIR, 'mot_analysis', [cond '_CONFOUND.par']);
+confound = dlmread(confound);
+confound = sum(confound, 2);
+
+confound(confound > 1) = 1; % binarize the vector
+confound = find(confound);
+
+
+fmri = load_nii( fullfile(TOP_DIR, 'task_data', 'preproc', ['scrubbed_motreg_snlmt_' cond '.nii']) );
+
+cfmri = fmri;
+
+cfmri.img(:,:,:,confound) = [];
+cfmri.hdr.dime.dim(5)     = size(cfmri.img, 4);
+
+save_nii(cfmri, fullfile(TOP_DIR, 'task_data', 'preproc', ['rm_' cond '.nii']) )
---
+++ rm_vols.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -M hpc3586@localhost
+#$ -m be
+#$ -q abaqus.q
+
+TOP_DIR=$1
+cond=$2
+output=$3
+
+##### prep arguments for passage into Matlab #####
+
+
+mTOP_DIR=$(echo "TOP_DIR='$TOP_DIR'")
+mCOMD=$(echo "cond='$cond'")
+mOUT=$(echo "output=$output")
+
+mCOMMANDS=$(echo "$mTOP_DIR;$mCOND;$mOUT"	)
+
+matlab -r "$mCOMMANDS" -nosplash -nodesktop -nosoftwareopengl -wait
+
