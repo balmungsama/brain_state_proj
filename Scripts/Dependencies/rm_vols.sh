ab6b6bd1a9840654ad406eb85b0a9d336bb5f078
BalmungSama <jbryaneusebio@gmail.com>
2017-05-29 19:57:06 -0400

    added lines to run melodic

--- Scripts/Dependencies/rm_vols.sh
+++ Scripts/Dependencies/rm_vols.sh
@@ -11,7 +11,7 @@ output=$3
 script_path=$4
 
 ##### prep arguments for passage into Matlab #####
-
+subj=$(basename $TOP_DIR)
 
 mTOP_DIR=$(echo "TOP_DIR='$TOP_DIR'")
 mCOND=$(echo "cond='$cond'")
@@ -21,5 +21,19 @@ mRM_VOLS=$(echo "run('rm_vols.m')")
 
 mCOMMANDS=$(echo "$mTOP_DIR;$mCOND;$mOUT;$mSCRIPT;$mRM_VOLS"	)
 
-matlab -r "$mCOMMANDS" -nosplash -nodesktop -nosoftwareopengl -wait
+matlab -r "$mCOMMANDS" -nosplash -nodesktop -nosoftwareopengl #-wait
 
+qstat  | grep "roi_" > $subj'_job_list.txt'
+line_count=($(wc -l $subj'_job_list.txt'))
+
+if [[ line_count == 1 ]]; then
+ files=$(ls $TOP_DIR/../*/task_data/preproc/rm_$cond.nii)
+
+	for file in $files; do 
+		list=$(echo $list $file)
+	done
+
+	mkdir -o $output/$cond
+	
+	melodic -i $list -o $output/$cond/
+fi
