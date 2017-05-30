38693a458073abf834fdedd7a322d735c56da3da
BalmungSama <jbryaneusebio@gmail.com>
2017-05-29 20:12:30 -0400

    removed unnecessary lines & added disp statements

--- Scripts/Dependencies/rm_vols.m
+++ Scripts/Dependencies/rm_vols.m
@@ -1,10 +1,3 @@
-
-
-
-disp(['TOP_DIR = ', TOP_DIR]);
-disp(['cond = ', cond]);
-disp(['output = ', output]);
-
 addpath( genpath('/home/hpc3586/matlab_plugins') );
 
 confound = fullfile(TOP_DIR, 'mot_analysis', [cond '_CONFOUND.par']);
@@ -14,12 +7,16 @@ confound = sum(confound, 2);
 confound(confound > 1) = 1; % binarize the vector
 confound = find(confound);
 
-
+disp('Loading nifti file...');
 fmri = load_nii( fullfile(TOP_DIR, 'task_data', 'preproc', ['scrubbed_motreg_snlmt_' cond '.nii']) );
 
 cfmri = fmri;
 
+disp('Removing scrubbed volumes...');
 cfmri.img(:,:,:,confound) = [];
 cfmri.hdr.dime.dim(5)     = size(cfmri.img, 4);
 
+disp('Saving the new nifti file...');
 save_nii(cfmri, fullfile(TOP_DIR, 'task_data', 'preproc', ['rm_' cond '.nii']) )
+
+disp('DONE');
