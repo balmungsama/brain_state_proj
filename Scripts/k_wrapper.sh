90d96de3df9a19bd8cf60f0f94a49b8a1a950d70
BalmungSama <jbryaneusebio@gmail.com>
2017-06-07 02:25:44 -0400

    fixed ICA sge_output file name

--- Scripts/k_wrapper.sh
+++ Scripts/k_wrapper.sh
@@ -64,7 +64,7 @@ case $DATA in
 
 			done
 
-			qsub -N ICA_$cond -e $script_path/logs/$DATE/ica_$cond.err -o $script_path/logs/$DATE/ica_$name.out -hold_jid /roi_ $script_path/Dependencies/ICA_ROI.sh $path $cond $output $script_path $nROI $mode
+			qsub -N ICA_$cond -e $script_path/logs/$DATE/ica_$cond.err -o $script_path/logs/$DATE/ica_$cond.out -hold_jid /roi_ $script_path/Dependencies/ICA_ROI.sh $path $cond $output $script_path $nROI $mode
 
 		else
 				echo "Please enter either 'subj' or 'group'"
