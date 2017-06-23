4a5f370b03ee755361685a7e6df2e2765a913f00
BalmungSama <jbryaneusebio@gmail.com>
2017-06-23 09:36:58 -0400

    fixed bad file names

--- Preprocessing/outlier_detect.sh
+++ Preprocessing/outlier_detect.sh
@@ -11,7 +11,7 @@ echo '	computing DVARS...'
 mkdir $SUBJ_DIR/mot_analysis
 mkdir $SUBJ_DIR/mot_analysis/plots
 
-NIFTI_FILE=$SUBJ_DIR/task_data/preproc/motreg_snlmt_$COND
+NIFTI_FILE=$SUBJ_DIR/task_data/preproc/snlmt_$COND
 
 # DVARS
 fsl_motion_outliers -i $NIFTI_FILE -o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' -s $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS' --dvars --nomoco --thresh=$DVARS
