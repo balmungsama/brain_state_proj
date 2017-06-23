17cea55e6aa902a883978869557db52f0363d6b0
BalmungSama <jbryaneusebio@gmail.com>
2017-06-23 09:26:16 -0400

    don't need the deg2mm.R call

--- Preprocessing/motcor.sh
+++ Preprocessing/motcor.sh
@@ -14,4 +14,4 @@ mcflirt -in $NIFTI_file -o $SUBJ_DIR/task_data/preproc/mt_$COND -refvol 1 -plots
 
 mv $SUBJ_DIR/task_data/preproc/mt_$COND.par $SUBJ_DIR/MPEs/$COND.1D
 
-Rscript $PREPROC/deg2mm.R --PATH=$SUBJ_DIR --COND=$COND
+# Rscript $PREPROC/deg2mm.R --PATH=$SUBJ_DIR --COND=$COND
