ec1e166b6cebfb0153eac0da375f94067ea51dd4
BalmungSama <jbryaneusebio@gmail.com>
2017-06-25 17:39:28 -0400

    added echo statement

--- Preprocessing/outlier_detect.sh
+++ Preprocessing/outlier_detect.sh
@@ -14,6 +14,8 @@ mkdir $SUBJ_DIR/mot_analysis/plots
 # DVARS
 fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/mt_$COND.nii* -o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' -s $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS' --dvars --nomoco --thresh=$DVARS
 
+echo '	computing FD...'
+
 # FD
 fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd --thresh=$FD
 
