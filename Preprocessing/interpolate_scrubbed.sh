79aa7ddbc7e5062de95d6bb863b8fa57935ef6db
BalmungSama <jbryaneusebio@gmail.com>
2017-06-25 20:50:34 -0400

    omitting matlab startup text

--- Preprocessing/interpolate_scrubbed.sh
+++ Preprocessing/interpolate_scrubbed.sh
@@ -4,4 +4,4 @@ PREPROC=$3
 
 echo '	Interpolatng flagged volumes... '
 
-matlab -nodesktop -nosplash -r "SUBJ_DIR='$SUBJ_DIR';COND='$COND';run('$PREPROC/interpolate_scrubbed.m')"
+matlab -nodesktop -nosplash -r "SUBJ_DIR='$SUBJ_DIR';COND='$COND';run('$PREPROC/interpolate_scrubbed.m')" | tail +11
