476ce76946dd705abc5689ccdd0b199e335ac3eb
BalmungSama <jbryaneusebio@gmail.com>
2017-06-26 00:12:27 -0400

    if the PASS file exists, then uneccessary functions will be skipped over

--- Preprocessing/preproc_wrapper.sh
+++ Preprocessing/preproc_wrapper.sh
@@ -21,16 +21,20 @@ bash $PREPROC/skullstrip.sh $SUBJ_DIR $COND $PREPROC
 bash $PREPROC/slicetime.sh $SUBJ_DIR $COND $PREPROC
 bash $PREPROC/motcor.sh $SUBJ_DIR $COND $PREPROC
 bash $PREPROC/outlier_detect.sh $SUBJ_DIR $COND $PREPROC $FD $DVARS
-Rscript $PREPROC/mk_scrub_mat.R --PATH=$SUBJ_DIR --COND=$COND --RM=$RM #--NUISSANCE=$NUISSANCE
 
-bash $PREPROC/interpolate_scrubbed.sh $SUBJ_DIR $COND $PREPROC
-bash $PREPROC/bandpass_filter.sh $SUBJ_DIR $COND $LOW $HIGH
-bash $PREPROC/mot_reg.sh $SUBJ_DIR $COND
+if [ -e $SUBJ_DIR/PASS ]; then
+	PASS=TRUE
+fi
 
-bash $PREPROC/normalization.sh $SUBJ_DIR $COND $PREPROC
-bash $PREPROC/nuis_reg.sh $SUBJ_DIR $COND $PREPROC
-
-bash $PREPROC/mot_scrubbing.sh $SUBJ_DIR $COND $PREPROC
+if [ PASS != TRUE ]; then
+	Rscript $PREPROC/mk_scrub_mat.R --PATH=$SUBJ_DIR --COND=$COND --RM=$RM #--NUISSANCE=$NUISSANCE
+	bash $PREPROC/interpolate_scrubbed.sh $SUBJ_DIR $COND $PREPROC
+	bash $PREPROC/bandpass_filter.sh $SUBJ_DIR $COND $LOW $HIGH
+	bash $PREPROC/mot_reg.sh $SUBJ_DIR $COND
+	bash $PREPROC/normalization.sh $SUBJ_DIR $COND $PREPROC
+	bash $PREPROC/nuis_reg.sh $SUBJ_DIR $COND $PREPROC
+	bash $PREPROC/mot_scrubbing.sh $SUBJ_DIR $COND $PREPROC
+fi
 
 # bash $PREPROC/rm_intermediate_files.sh $SUBJ_DIR $COND
 
