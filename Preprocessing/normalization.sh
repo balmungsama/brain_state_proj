55a14c130bedf20adde42530b5655d4bec578b6f
BalmungSama <jbryaneusebio@gmail.com>
2017-06-25 21:02:01 -0400

    updating for compatibility between scripts

--- Preprocessing/normalization.sh
+++ Preprocessing/normalization.sh
@@ -13,7 +13,7 @@ mkdir $SUBJ_DIR/task_data/preproc/mats
 echo '	Spatial normalization...'
 
 echo '       + Linear-warping functional to structural...'
-flirt -ref $SUBJ_DIR/anatom/brain_Mprage.nii.gz -in $SUBJ_DIR/task_data/preproc/filt_interop_mt_$COND -omat $SUBJ_DIR/task_data/preproc/mats/func2str.mat -dof 6
+flirt -ref $SUBJ_DIR/anatom/brain_Mprage.nii.gz -in $SUBJ_DIR/task_data/preproc/mot_filt_interop_mt_$COND -omat $SUBJ_DIR/task_data/preproc/mats/func2str.mat -dof 6
 echo '       + Linear-warping structural to standard template...'
 flirt -ref $TEMPLATE -in $SUBJ_DIR/anatom/brain_Mprage.nii.gz -omat $SUBJ_DIR/anatom/mats/aff_str2std.mat -out $SUBJ_DIR/anatom/l_brain_Mprage
 echo '       + Non-linear-warping structural to standard template...'
@@ -21,9 +21,9 @@ fnirt --ref=$TEMPLATE --in=$SUBJ_DIR/anatom/brain_Mprage.nii.gz --aff=$SUBJ_DIR/
 echo '       + Creating binary mask from non-linearly warped image...'
 fslmaths $SUBJ_DIR/anatom/nl_brain_Mprage -bin $SUBJ_DIR/anatom/bin_nl_brain_Mprage
 echo '       + Applying standardized warp to functional data...'
-applywarp --ref=$TEMPLATE --in=$SUBJ_DIR/task_data/preproc/filt_interop_mt_$COND --out=$SUBJ_DIR/task_data/preproc/nl_filt_interop_mt_$COND --warp=$SUBJ_DIR/anatom/cout_nl_brain_Mprage --premat=$SUBJ_DIR/task_data/preproc/mats/func2str.mat
+applywarp --ref=$TEMPLATE --in=$SUBJ_DIR/task_data/preproc/mot_filt_interop_mt_$COND --out=$SUBJ_DIR/task_data/preproc/nl_mot_filt_interop_mt_$COND --warp=$SUBJ_DIR/anatom/cout_nl_brain_Mprage --premat=$SUBJ_DIR/task_data/preproc/mats/func2str.mat
 echo '       + Applying spatial smoothing kernel...'
-fslmaths $SUBJ_DIR/task_data/preproc/nl_filt_interop_mt_$COND -kernel gauss 2.54798709 -fmean $SUBJ_DIR/task_data/preproc/snl_filt_interop_mt_$COND
+fslmaths $SUBJ_DIR/task_data/preproc/nl_mot_filt_interop_mt_$COND -kernel gauss 2.54798709 -fmean $SUBJ_DIR/task_data/preproc/snl_mot_filt_interop_mt_$COND
 
 
 
