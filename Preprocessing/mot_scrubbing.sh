e0f35230bbc5593b90c89bfa5743f0a38b734f0b
BalmungSama <jbryaneusebio@gmail.com>
2017-06-26 00:41:50 -0400

    remove the scrub_nuis_snl_mot_filt_interop_mt_$COND.nii file

--- Preprocessing/mot_scrubbing.sh
+++ Preprocessing/mot_scrubbing.sh
@@ -7,3 +7,5 @@ fsl_glm -i $SUBJ_DIR/task_data/preproc/nuis_snl_mot_filt_interop_mt_$COND -d $SU
 
 rm $SUBJ_DIR/task_data/preproc/nuis_snl_mot_filt_interop_mt_$COND.nii*
 gzip $SUBJ_DIR/task_data/preproc/scrub_nuis_snl_mot_filt_interop_mt_$COND.nii
+
+rm $SUBJ_DIR/task_data/preproc/scrub_nuis_snl_mot_filt_interop_mt_$COND.nii
