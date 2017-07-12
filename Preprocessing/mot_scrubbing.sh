bea9594f27f52020606cde771f2e1e9ea4596faa
BalmungSama <jbryaneusebio@gmail.com>
2017-07-12 14:25:49 -0400

    hopefully fixed the problem in the motion scrubbing step

--- Preprocessing/mot_scrubbing.sh
+++ Preprocessing/mot_scrubbing.sh
@@ -5,7 +5,7 @@ stage=$3
 if [[ "$stage" == 'init' ]]; then
 	
 	echo '	Motion scrubbing (init) ...'
-	fsl_glm -i $SUBJ_DIR/task_data/preproc/snl_norm_mt_$COND -d $SUBJ_DIR/mot_analysis/$COND'_CONFOUND.par' --out_res=$SUBJ_DIR/task_data/preproc/scrub_snl_norm_mt_$COND
+	fsl_glm -i $SUBJ_DIR/task_data/preproc/snl_norm_mt_$COND -d $SUBJ_DIR/mot_analysis/$COND'_CONFOUND.par' --out_res=$SUBJ_DIR/task_data/preproc/scrub_snl_norm_mt_$COND.nii
 
 	echo '	Motion censoring ...'
 	matlab -nodesktop -nosplash -r "SUBJ_DIR='$SUBJ_DIR';COND='$COND';run('$PREPROC/mot_censor.m')" 
@@ -13,6 +13,6 @@ if [[ "$stage" == 'init' ]]; then
 elif [[ "$stage" == 'fin' ]]; then
 	
 	echo '	Motion scrubbing (fin) ...'
-	fsl_glm -i $SUBJ_DIR/task_data/preproc/filt_interp_nuis_snl_norm_mt_$COND -d $SUBJ_DIR/mot_analysis/$COND'_CONFOUND.par' --out_res=$SUBJ_DIR/task_data/preproc/scrub_filt_interp_nuis_snl_norm_mt_$COND
+	fsl_glm -i $SUBJ_DIR/task_data/preproc/filt_interp_nuis_snl_norm_mt_$COND -d $SUBJ_DIR/mot_analysis/$COND'_CONFOUND.par' --out_res=$SUBJ_DIR/task_data/preproc/scrub_filt_interp_nuis_snl_norm_mt_$COND.nii
 
 fi
