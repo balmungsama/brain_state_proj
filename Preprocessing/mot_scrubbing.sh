ba78ca1477a52302ac875e100416dce2ac7b791d
BalmungSama <jbryaneusebio@gmail.com>
2017-07-19 13:39:43 -0400

    fixed gzipping

--- Preprocessing/mot_scrubbing.sh
+++ Preprocessing/mot_scrubbing.sh
@@ -47,9 +47,9 @@ elif [[ "$stage" == 'fin' ]]; then
 
 	matlab -nodesktop -nosplash -r "SUBJ_DIR='$SUBJ_DIR';COND='$COND';FIN=true;run('$PREPROC/mot_censor.m')"
 
-	gzip scrub_filt_interp_nuis_snl_norm_mt_$COND.nii
-	gzip censor_filt_interp_nuis_snl_norm_mt_$COND.nii
-
+	gzip $SUBJ_DIR/task_data/preproc/scrub_filt_interp_nuis_snl_norm_mt_$COND.nii
+	gzip $SUBJ_DIR/task_data/preproc/censor_filt_interp_nuis_snl_norm_mt_$COND.nii
+	gzip $SUBJ_DIR/task_data/preproc/nuis_snl_norm_mt_$COND.nii
 	##### removing intermediate files #####
 
 	rm $SUBJ_DIR/task_data/preproc/filt_interp_nuis_snl_norm_mt_$COND.nii
