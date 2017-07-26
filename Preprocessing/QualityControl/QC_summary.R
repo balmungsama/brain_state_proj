a479a754307d2904a3d5e2548ef60fda985261bc
BalmungSama <jbryaneusebio@gmail.com>
2017-07-26 18:03:41 -0400

    fixing the issue when saving the output

--- Preprocessing/QualityControl/QC_summary.R
+++ Preprocessing/QualityControl/QC_summary.R
@@ -33,22 +33,24 @@ for (arg in args) {
 }
 
 ##### import sample data #####
-sample.data <- NULL
+sample.data <- list()
 
 sample.data[['DMN']] <- read.csv( file.path(PREPROC, 'QualityControl', 'DMN_reference.csv'), header = T, row.names = 1 )
+sample.data[['DMN']] <- as.matrix( sample.data[['DMN']] )
 
 ##### import subject data #####
 
-subj.data <- NULL
+subj.data <- list()
 
 subj.data[['subj_id']]    <- basename(SUBJ_DIR)
 subj.data[['FC_matrix']]  <- read.csv( file.path(SUBJ_DIR, 'QualityControl', paste0(COND, '_DMN_FC.csv')), header = T, row.names = 1 )
+subj.data[['FC_matrix']]  <- as.matrix(subj.data[['FC_matrix']])
 
 ##### check overlap with DMN #####
 
 QC.summary <- list()
 
-QC.summary[['correl_with_DMN']] <- cor.test(sample.data[['DMN']], subj.data[['FC_matrix']])
+QC.summary[['correl_with_DMN']] <- cor.test(sample.data[['DMN']], subj.data[['FC_matrix']])$estimate
 
 ##### save output #####
 
