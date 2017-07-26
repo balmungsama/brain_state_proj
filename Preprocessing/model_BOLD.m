5bd27c395a7eb6615b98955d897337bde060eadf
BalmungSama <jbryaneusebio@gmail.com>
2017-07-26 16:45:17 -0400

    added masking to the modelling of nuisance bold signals

--- Preprocessing/model_BOLD.m
+++ Preprocessing/model_BOLD.m
@@ -1,3 +1,5 @@
+[status, FSLDIR] = system('echo $FSLDIR');
+
 %%%%%%%%%%%%%%%%%%%%%%
 %% trouble-shooting %%
 %%%%%%%%%%%%%%%%%%%%%%
@@ -6,26 +8,32 @@ disp('model_BOLD.m')
 disp( ['SUBJ_DIR = ', SUBJ_DIR ] );
 disp( ['COND = ', COND ] );
 
+
 %%%%%%%%%%
 %% main %%
 %%%%%%%%%%
 
 cd(SUBJ_DIR);
 
-BETAS      = load_nii([ 'nuisance/BETAS_' COND '.nii' ]);
+BETAS      = load_nii([ 'nuisance/BETAS_' COND '.nii'                ]);
 subj_nifti = load_nii([ 'task_data/preproc/snl_norm_mt_' COND '.nii' ]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%% experimental start %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-grand_mean = mean2(subj_nifti.img) ;
-grand_sd   =  std2(subj_nifti.img) ;
+subj_mask     = load_nii([ FSLDIR '/data/standard/MNI152_T1_2mm_brain_mask.nii.gz' ]);
+subj_mask.img = double(subj_mask.img) ;
 
-tmp_subj_nifti = subj_nifti;
+for ii = 1:size(subj_nifti.img,4)
+	masked_nifti.img(:,:,:, ii) = subj_nifti.img(:,:,:, ii) .* subj_mask.img ;
+end
 
-tmp_subj_nifti.img = tmp_subj_nifti.img - grand_mean ;
-tmp_subj_nifti.img = tmp_subj_nifti.img / grand_sd   ;
+grand_mean = mean2(masked_nifti.img) ;
+grand_sd   =  std2(masked_nifti.img) ;
+
+subj_nifti.img = subj_nifti.img - grand_mean ;
+subj_nifti.img = subj_nifti.img / grand_sd   ;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%% experimental end %%%%%
@@ -38,15 +46,21 @@ for vol = 1:size(subj_nifti.img, 4)
 	% disp([ 'VOL  ', num2str(vol) ])
 
 	for beta = 1:size(BETAS.img, 4)
-		estim_tcourse(:,:,:, vol) = estim_tcourse(:,:,:, vol) + ( tmp_subj_nifti.img(:,:,:, vol) .* BETAS.img(:,:,:, beta) );
 		
-		% disp( estim_tcourse(30, 60, 40, vol) )
+		estim_tcourse(:,:,:, vol) = estim_tcourse(:,:,:, vol) + ( subj_nifti.img(:,:,:, vol) .* BETAS.img(:,:,:, beta) );
+	
 	end
 	
 end
 
-subj_nifti.img = tmp_subj_nifti.img - estim_tcourse;
+subj_nifti.img = subj_nifti.img - estim_tcourse;
 
-save_nii(subj_nifti, [ 'task_data/preproc/nuis_snl_norm_mt_' COND '.nii' ]);
+for ii = 1:size(subj_nifti.img,4)
+	subj_nifti.img(:,:,:, ii) = subj_nifti.img(:,:,:, ii) .* subj_mask.img ;
+end
+
+subj_nifti.img(isnan(subj_nifti.img)) = 0 ;
+
+save_nii(subj_nifti, [ 'task_data/preproc/nuis_snl_norm_mt_' COND '.nii' ]) ;
 
 exit
--- Preprocessing/nuis_reg.sh
+++ Preprocessing/nuis_reg.sh
@@ -14,11 +14,11 @@ fslmeants -i $SUBJ_DIR/task_data/preproc/censor_snl_norm_mt_$COND -m $SUBJ_DIR/a
 
 paste $SUBJ_DIR/nuisance/$COND'_t_vent.txt' $SUBJ_DIR/nuisance/$COND'_t_wm.txt' $SUBJ_DIR/nuisance/$COND'_t_gs.txt'  > $SUBJ_DIR/nuisance/$COND'_NUISANCE'.txt
 
-Rscript $PREPROC/center_nuisance.R --PATH=$SUBJ_DIR --COND=$COND
+# Rscript $PREPROC/center_nuisance.R --PATH=$SUBJ_DIR --COND=$COND
 
-fsl_glm -i $SUBJ_DIR/task_data/preproc/censor_snl_norm_mt_$COND -d $SUBJ_DIR/nuisance/$COND'_regressors'.txt -o $SUBJ_DIR/nuisance/BETAS_$COND -m $SUBJ_DIR/anatom/bin_nl_brain_Mprage --demean --dat_norm
+fsl_glm -i $SUBJ_DIR/task_data/preproc/censor_snl_norm_mt_$COND -d $SUBJ_DIR/nuisance/$COND'_NUISANCE'.txt -o $SUBJ_DIR/nuisance/BETAS_$COND -m $SUBJ_DIR/anatom/bin_nl_brain_Mprage --demean --dat_norm --desnorm
 
-matlab -nodesktop -nosplash -r "SUBJ_DIR='$SUBJ_DIR';COND='$COND';run('$PREPROC/model_BOLD.m')" 
+matlab -nodesktop -nosplash -r "FSLDIR='$FSLDIR';SUBJ_DIR='$SUBJ_DIR';COND='$COND';run('$PREPROC/model_BOLD.m')" 
 
 ##### removing intermediate files #####
 
