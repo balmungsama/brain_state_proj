ccc2148551b94b63fd40bf18196d43c12e646308
John Eusebio <jbryaneusebio@gmail.com>
2018-05-19 17:55:10 -0400

    motion correction will now correct to the middle volume

--- Preprocessing/motcor.sh
+++ Preprocessing/motcor.sh
@@ -4,8 +4,10 @@ PREPROC=$3		 # NOTE: must use this name for behav & fmri data
 					     
 mkdir $SUBJ_DIR/MPEs
 
+nvols=$(fslnvols $SUBJ_DIR/task_data/$COND.nii*)
+
 echo '	motion correction...'
 
-mcflirt -in $SUBJ_DIR/task_data/preproc/t_$COND.nii -o $SUBJ_DIR/task_data/preproc/mt_$COND -refvol 1 -plots
+mcflirt -in $SUBJ_DIR/task_data/preproc/t_$COND.nii -o $SUBJ_DIR/task_data/preproc/mt_$COND -refvol $(($nvols/2)) -plots
 
 mv $SUBJ_DIR/task_data/preproc/mt_$COND.par $SUBJ_DIR/MPEs/$COND.1D
