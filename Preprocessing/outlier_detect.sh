e106ccdb092129ed898bd18a186a2940d64a18f6
John Eusebio <jbryaneusebio@gmail.com>
2018-05-20 02:34:05 -0400

    add support for the fd outlier detection

--- Preprocessing/outlier_detect.sh
+++ Preprocessing/outlier_detect.sh
@@ -15,7 +15,9 @@ mkdir $SUBJ_DIR/mot_analysis/plots
 
 # DVARS
 fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND -s $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS' --dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage
-Rscript $PREPROC/find_dvars_thresh.R --PATH=$SUBJ_DIR --COND=$COND
+dvars_thr=$(Rscript $PREPROC/find_dvars_thresh.R --PATH=$SUBJ_DIR --COND=$COND)
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND -o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' --dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage --thresh=$dvars_thr
+
 
 # fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND -o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' -s $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS' --dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage --thresh=$DVARS
 
@@ -31,7 +33,10 @@ fi
 echo '	computing FD...'
 
 # FD
-fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd --thresh=$FD
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd 
+fd_thr=$(Rscript $PREPROC/find_fd_thresh.R --PATH=$SUBJ_DIR --COND=$COND)
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' --dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage --thresh=$fd_thr
+
 
 if [[ ! -e $SUBJ_DIR/mot_analysis/$COND'_FD.par' ]]; then
 
