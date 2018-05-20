0a18aec4c330bced0725ea1dd84c922ef9647361
John Eusebio <jbryaneusebio@gmail.com>
2018-05-20 02:50:41 -0400

    fixed teh fd outlier detection

--- Preprocessing/outlier_detect.sh
+++ Preprocessing/outlier_detect.sh
@@ -33,9 +33,9 @@ fi
 echo '	computing FD...'
 
 # FD
-fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd 
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd 
 fd_thr=$(Rscript $PREPROC/find_fd_thresh.R --PATH=$SUBJ_DIR --COND=$COND)
-fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' --dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage --thresh=$fd_thr
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd --thresh=$fd_thr
 
 
 if [[ ! -e $SUBJ_DIR/mot_analysis/$COND'_FD.par' ]]; then
