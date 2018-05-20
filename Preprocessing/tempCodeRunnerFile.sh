3d2a1d2b3b8d9133d265c97a0ca717e3d3224075
John Eusebio <jbryaneusebio@gmail.com>
2018-05-20 03:13:56 -0400

    whatever

--- Preprocessing/tempCodeRunnerFile.sh
+++ Preprocessing/tempCodeRunnerFile.sh
@@ -1,1 +1,1 @@
-PREPROC='/global/home/hpc3586/JE_packages/brain_state_proj/Preprocessing'
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/t_$COND.nii* -o $SUBJ_DIR/mot_analysis/$COND'_FD.par' -s $SUBJ_DIR/mot_analysis/$COND'_FD.val' -p $SUBJ_DIR/mot_analysis/plots/$COND'_FD' --fd --thresh=$fd_thr
