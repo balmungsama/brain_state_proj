b77cfb319a40703ed404f99dcbe537e38fcd0188
balmungsama <jbryaneusebio@gmail.com>
2018-07-09 16:23:56 -0400

    multiple files error

--- Preprocessing/outlier_detect.sh
+++ Preprocessing/outlier_detect.sh
@@ -13,17 +13,17 @@ mkdir $SUBJ_DIR/mot_analysis
 mkdir $SUBJ_DIR/mot_analysis/plots
 
 # DVARS
-fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND \
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND.nii.gz \
 	-o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' \
 	-s $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' \
 	-p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS' \
-	--dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage.nii.gz
+	--dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage
 dvars_thr=$(Rscript $PREPROC/find_dvars_thresh.R --PATH=$SUBJ_DIR --COND=$COND)
-fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND \
+fsl_motion_outliers -i $SUBJ_DIR/task_data/preproc/dsnl_norm_mt_$COND.nii.gz \
 	-o $SUBJ_DIR/mot_analysis/$COND'_DVARS.par' \
 	-s $SUBJ_DIR/mot_analysis/$COND'_DVARS.val' \
 	-p $SUBJ_DIR/mot_analysis/plots/$COND'_DVARS' \
-	--dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage.nii.gz \
+	--dvars --nomoco -m $SUBJ_DIR/anatom/dbin_nl_brain_Mprage \
 	--thresh=$dvars_thr
 
 echo DVARS threshold = $dvars_thr
